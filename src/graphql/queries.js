387d27a63b7d47ecb975d3308783abf80972bcdf
“杨朋飞” <yours910809@163.com>
2021-08-18 17:21:22 +0800

    数据库更新

---
+++ src/graphql/queries.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNote = /* GraphQL */ `
+  query GetNote($id: ID!) {
+    getNote(id: $id) {
+      id
+      name
+      description
+      image
+      productChName
+      productNo
+      productEnName
+      productSize
+      ProductImageName
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listNotes = /* GraphQL */ `
+  query ListNotes(
+    $filter: ModelNoteFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotes(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        image
+        productChName
+        productNo
+        productEnName
+        productSize
+        ProductImageName
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
