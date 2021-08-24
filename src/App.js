a66981fef38d2ab71c3e994b1f7327f0a72d0920
“杨朋飞” <yours910809@163.com>
2021-08-24 10:03:06 +0800

    提交

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { listNotes } from './graphql/queries';
 import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-const initialFormState = { name: '', description: '' }
-
+const initialFormState = { name: '', description: '',productChName:'',productNo:'',productEnName:'',productSize:'' }
+var imageArrayName = []
 function App() {
   const [notes, setNotes] = useState([]);
   const [formData, setFormData] = useState(initialFormState);
@@ -16,20 +16,27 @@ function App() {
   }, []);
 
 
+  //选择图片
   async function onChange(e) {
-    if (!e.target.files[0]) return
-    const file = e.target.files[0];
-    setFormData({ ...formData, image: file.name });
-    await Storage.put(file.name, file);
+    imageArrayName = []
+    for(var i = 0;i<e.target.files.length;i++){
+      const file = e.target.files[i];
+      imageArrayName.push(file.name)
+      console.log(imageArrayName)
+    }
+    var jsonStr = JSON.stringify(imageArrayName)
+    setFormData({ ...formData, image: jsonStr});
+    for(var i = 0;i<e.target.files.length;i++){
+      await Storage.put(imageArrayName[i], e.target.files[i]);  
+    }
     fetchNotes();
   }
-  
-
-
+  //检索数据库数据
   async function fetchNotes() {
     const apiData = await API.graphql({ query: listNotes });
     const notesFromAPI = apiData.data.listNotes.items;
     await Promise.all(notesFromAPI.map(async note => {
+      console.log("测试存储数据图片image     " +note.image)
       if (note.image) {
         const image = await Storage.get(note.image);
         note.image = image;
@@ -38,61 +45,96 @@ function App() {
     }))
     setNotes(apiData.data.listNotes.items);
   }
-
+  //上传数据啊啊
   async function createNote() {
-    if (!formData.name || !formData.description) return;
+    // if (!formData.name || !formData.description) return;
+    // console.log("图       "+formData.image)
+    
     await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+
+    //查看上传数据结果
+    var imageArray = []
     if (formData.image) {
-      const image = await Storage.get(formData.image);
-      formData.image = image;
+      for(var i = 0;i<formData.image.length;i++){
+        const image = await Storage.get(formData.image[i]);
+        imageArray.push(image);   
+        console.log("图jieguo 前3333       "+image)
+      }
+      formData.image = imageArray;
     }
+
     setNotes([ ...notes, formData ]);
     setFormData(initialFormState);
   }
-  
-
   async function deleteNote({ id }) {
     const newNotesArray = notes.filter(note => note.id !== id);
     setNotes(newNotesArray);
     await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
   }
-
   return (
     <div className="App">
-      <h1>My Notes App</h1>
-      <input
+      <h1>潮牌 产品数据上传</h1>
+      {/* <input style ={{height:40}}
         onChange={e => setFormData({ ...formData, 'name': e.target.value})}
-        placeholder="Note name"
+        placeholder="Product name"
         value={formData.name}
       />
-      <input
+      <input style ={{height:40,marginLeft:10}}
         onChange={e => setFormData({ ...formData, 'description': e.target.value})}
-        placeholder="Note description"
+        placeholder="Product description"
         value={formData.description}
+      /> */}
+
+      <input style ={{height:40,marginLeft:10}}
+        onChange={e => setFormData({ ...formData, 'productChName': e.target.value})}
+        placeholder="Product productChName"
+        value={formData.productChName}
+      />
+      <input style ={{height:40,marginLeft:10}}
+        onChange={e => setFormData({ ...formData, 'productEnName': e.target.value})}
+        placeholder="Product productEnName"
+        value={formData.productEnName}
       />
 
-      <input
-        type="file"
-        onChange={onChange}
+      <input style ={{height:40,marginLeft:10}}
+        onChange={e => setFormData({ ...formData, 'productNo': e.target.value})}
+        placeholder="Product productNo"
+        value={formData.productNo}
+      />
+      <input style ={{height:40,marginLeft:10}}
+        onChange={e => setFormData({ ...formData, 'productSize': e.target.value})}
+        placeholder="Product productSize"
+        value={formData.productSize}
       />
 
-      <button onClick={createNote}>Create Note</button>
-      <div style={{marginBottom: 30}}>
-      {
-  notes.map(note => (
-    <div key={note.id || note.name}>
-      <h2>{note.name}</h2>
-      <p>{note.description}</p>
-      <button onClick={() => deleteNote(note)}>Delete note</button>
-      {
-        note.image && <img src={note.image} style={{width: 400}} />
+      <div style={{marginBottom: 100}}>
+        <input style ={{height:40,marginLeft:20,marginTop:50}}
+          type="file"
+          multiple="multiple"
+          onChange={onChange}
+        />
+        <button style ={{height:40}} onClick={createNote}>Create Product</button>
+      </div >
+
+      <div style={{marginBottom: 100}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.productNo}>
+              <h2>{note.productChName}</h2>
+              <p>{note.productNo}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            {
+              // note.image && <img src={note.image} style={{width: 400}} />
+        }
+          </div>
+        ))
       }
-    </div>
-  ))
-}
+      </div >
 
-      </div>
-      <AmplifySignOut />
+
+      
+
+      <AmplifySignOut/>
     </div>
   );
 }
